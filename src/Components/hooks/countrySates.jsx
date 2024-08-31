a30a828f0ed0ff9a8b58c808c72c46392a3a9333
kumbhamAjay <ajaykumbam340@gmail.com>
2024-08-31 18:07:46 +0530

    update

--- src/Components/hooks/countrySates.jsx
+++ src/Components/hooks/countrySates.jsx
@@ -6,7 +6,7 @@ import { countries } from "../helpers.js/CountryStates";
 function CountryStates() {
   const [countrySelected, setCountrySelected] = useState("");
   const [stateSelected, setStateSelected] = useState("");
-  const [tableData,setTableData]=useState([])
+  const [tableData, setTableData] = useState([]);
   const changeCountryHandler = (e) => {
     // console.log(e.target.value);
     setCountrySelected(e.target.value);
@@ -15,38 +15,29 @@ function CountryStates() {
     // console.log(e.target.value);
     setStateSelected(e.target.value);
   };
-  const submitHandler=(e)=>{
-    e.preventDefault()
-    setTableData([...tableData,{Country:countrySelected,State:stateSelected}])
-
-  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTableData([
+      ...tableData,
+      { Country: countrySelected, State: stateSelected },
+    ]);
+    setCountrySelected("");
+    setStateSelected("");
+  };
   return (
     <>
-    <div>
-        <form action="" onSubmit={submitHandler}>
-      <label>Select Country</label>
-      <select
-        name="Country"
-        id=""
-        value={countrySelected}
-        onChange={changeCountryHandler}
-      >
-        <option hidden>Countries</option>
-        {Object.keys(countries).map((each, i) => {
-          return (
-            <option value={each} key={i}>
-              {each}
-            </option>
-          );
-        })}
-      </select><br />
-      {countrySelected && (
-        <>
-          <label>Select State</label>
-
-          <select name="" id="" onChange={changeStateHandler}>
-            <option hidden>States</option>
-            {countries[countrySelected].map((each, i) => {
+      <div style={{padding:"20px",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:'center',background:"#d9b3ff",width:'500px'}}>
+        <form action="" onSubmit={submitHandler} style={{padding:"20px"}}>
+          <label >Select Country:</label>
+          <select
+            name="Country"
+            id=""
+            value={countrySelected}
+            onChange={changeCountryHandler}
+            
+          >
+            <option hidden>Countries</option>
+            {Object.keys(countries).map((each, i) => {
               return (
                 <option value={each} key={i}>
                   {each}
@@ -54,13 +45,29 @@ function CountryStates() {
               );
             })}
           </select>
-        </>
-      )}<br/>
-      <button type="submit">Submit</button>
-      </form>
-    </div>
-    {tableData.length>0&&<CustomTable tableData={tableData}/>
-}
+          <br /><br />
+
+          <>
+            <label>Select State  :</label>
+
+            <select name="" id="" onChange={changeStateHandler}>
+              <option hidden>States</option>
+              {countrySelected &&
+                countries[countrySelected].map((each, i) => {
+                  return (
+                    <option value={each} key={i}>
+                      {each}
+                    </option>
+                  );
+                })}
+            </select>
+          </>
+          <br /><br />
+          <button type="submit">Submit</button>
+        </form>
+        {tableData.length > 0 && <CustomTable tableData={tableData} />}
+      </div>
+      
     </>
   );
 }
